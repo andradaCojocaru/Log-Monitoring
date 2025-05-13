625d4c4567627dfc35bddf8d642dd27241aa6c31
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2025-05-13 22:44:07 +0300

    Calculate the duration and create warning and errors

--- log_monitoring_app.py
+++ log_monitoring_app.py
@@ -14,24 +14,36 @@ if not os.path.exists(file_log_path):
 
 # Function to parse the log file
 def parse_log_file(file_path):
-    logs = []
+    logs = {}
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             timestamp, job_name, status, job_id = row
-            logs.append({
-                'timestamp': datetime.strptime(timestamp, '%H:%M:%S'),
-                'job_name': job_name,
-                'status': status,
-                'job_id': job_id
-            })
+            if job_id not in logs:
+                logs[job_id] = {}
+                logs[job_id]['job_name'] = job_name.strip()
+            if status.strip() == 'START':
+                logs[job_id]['start_time'] = datetime.strptime(timestamp, '%H:%M:%S')
+            elif status.strip() == 'END':
+                logs[job_id]['end_time'] = datetime.strptime(timestamp, '%H:%M:%S')
+                if 'start_time' in logs[job_id]:
+                    logs[job_id]['duration'] = logs[job_id]['end_time'] - logs[job_id]['start_time']
     return logs
 
+def calculate_durations(logs):
+    """Calculates the duration of each job and generates warnings/errors."""
+    for i in logs:
+        if 'duration' in logs[i]:
+            duration = logs[i]['duration']
+            if duration > ERROR_THRESHOLD:
+                print(f"ERROR: Job {logs[i]['job_name']} with PID {i} took {duration} (exceeds 10 minutes).")
+            elif duration > WARNING_THRESHOLD:
+                print(f"WARNING: Job {logs[i]['job_name']} with PID {i} took {duration} (exceeds 5 minutes).")
+    
 def main():
     logs =  parse_log_file(file_log_path)
-    print("Logs entries:")
-    for log in logs:
-        print(log)
+    calculate_durations(logs)
+    
 
 if __name__ == '__main__':
     main()
